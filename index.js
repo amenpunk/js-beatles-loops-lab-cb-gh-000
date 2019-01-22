fad4a0c8bf3a16d18622b8ff70e07b99de3ef74b
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-22 17:22:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function iLoveTheBeatles(number){
+    var song = [];
+    do{
+        song.push("I love the Beatles!");
+      number++;
+    }while(number < 15)
+    return song;
+}
+
+function theBeatlesPlay (musicians,instruments){
+  
+}
