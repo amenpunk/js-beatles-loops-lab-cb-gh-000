76b2f51b48c8b02bd544cddc74bfe19596beabb0
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-22 17:24:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,14 @@ function iLoveTheBeatles(number){
 }
 
 function theBeatlesPlay (musicians,instruments){
-  
+    var emp = [];
+    for(let i =0; musicians.length;i++){
+          if(i=0){
+           emp[i] = "John Lennon plays guitar";
+        }
+        else {
+          emp[i] = instruments;
+        }
+    }
+    return emp
 }
